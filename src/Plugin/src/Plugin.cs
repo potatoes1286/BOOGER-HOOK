50f81bda547f7232f8538b0547a0e58697d607cb
potatoes <526473+gb96@users.noreply.github.com>
2022-10-06 21:32:58 -0400

    initial commit

--- src/Plugin/src/Plugin.cs
+++ src/Plugin/src/Plugin.cs
@@ -1,4 +1,8 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
+using FistVR;
+using HarmonyLib;
+using UnityEngine;
 
 namespace H3VRMod
 {
@@ -6,27 +10,65 @@ namespace H3VRMod
 	[BepInProcess("h3vr.exe")]
 	public class Plugin : BaseUnityPlugin
 	{
-		private readonly Hooks _hooks;
 
-		public Plugin()
-		{
-			_hooks = new Hooks();
-			_hooks.Hook();
-		}
+		public static int booger_hookd = 0;
 
 		private void Awake()
 		{
-
+			Harmony.CreateAndPatchAll(typeof(Plugin));
 		}
 
-		private void Update()
+		[HarmonyPatch(typeof(FVRInteractiveObject), "BeginInteraction")]
+		[HarmonyPostfix]
+		public static void BOOGER_HOOK(FVRInteractiveObject __instance)
 		{
+			if (__instance.m_hand.Trigger_Touch.state)
+			{
+				if (__instance is FVRFireArm)
+				{
+					if (__instance is ClosedBoltWeapon)
+					{
+						var firearm = __instance as ClosedBoltWeapon;
+						ClosedBoltWeapon.FireSelectorModeType modeType = firearm.FireSelector_Modes[firearm.m_fireSelectorMode].ModeType;
+						if (firearm.Bolt.CurPos == ClosedBolt.BoltPos.Forward && modeType != ClosedBoltWeapon.FireSelectorModeType.Safe)
+						{
+							firearm.DropHammer();
+							firearm.m_hasTriggerReset = false;
+						}
+					}
 
-		}
+					if (__instance is OpenBoltReceiver)
+					{
+						var firearm = __instance as OpenBoltReceiver;
+						if (firearm.HasTriggerButton&&
+						    firearm.FireSelector_Modes[firearm.m_fireSelectorMode].ModeType != 0)
+						{
+							if (firearm.Bolt.CurPos == OpenBoltReceiverBolt.BoltPos.Forward)
+								return;
+							firearm.ReleaseSeer();
+						}
+					}
 
-		private void OnDestroy()
-		{
-			_hooks.Unhook();
+					if (__instance is Handgun)
+					{
+						var firearm = __instance as Handgun;
+						if(!firearm.IsSafetyEngaged)
+							firearm.ReleaseSeer();
+					}
+
+					if (__instance is BoltActionRifle)
+					{
+						var firearm = __instance as BoltActionRifle;
+						if (firearm.FireSelector_Modes[firearm.m_fireSelectorMode].ModeType != 0
+						    && firearm.BoltHandle.HandleState == BoltActionRifle_Handle.BoltActionHandleState.Forward
+						    && firearm.BoltHandle.HandleRot != 0)
+						{
+							firearm.DropHammer();
+						}
+					}
+				}
+			}
 		}
+		//public SteamVR_Action_Boolean Trigger_Touch; FVRViveHand
 	}
 }
--- src/Plugin/src/PluginInfo.cs
+++ src/Plugin/src/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace H3VRMod
 {
 	internal static class PluginInfo
 	{
-		internal const string NAME = "Example H3VR Mod";
-		internal const string GUID = "com.yourname.h3vrmod";
+		internal const string NAME = "BOOGER HOOK";
+		internal const string GUID = "dll.potatoes1286.keepyourboogerhookoffthetrigger";
 		internal const string VERSION = "1.0.0";
 	}
 }
